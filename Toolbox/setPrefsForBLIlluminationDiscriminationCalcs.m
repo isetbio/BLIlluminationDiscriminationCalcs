502e048531dc15d644887fe239f59b19c6a5a9fe
np_cottaris <cottaris@sas.upenn.edu>
2015-02-25 13:52:43 -0500

    Added comments.
    Signed-off-by: np_cottaris <cottaris@sas.upenn.edu>

--- Toolbox/setPrefsForBLIlluminationDiscriminationCalcs.m
+++ Toolbox/setPrefsForBLIlluminationDiscriminationCalcs.m
@@ -1,13 +1,23 @@
 function setPrefsForBLIlluminationDiscriminationCalcs
+% setPrefsForBLIlluminationDiscriminationCalcs
+%
+% Method to set the preferences for the BLIlluminationDiscriminationCalcs
+% project.
+%
+% 2/25/2015     npc     Wrote it.
+% 
 
     sharedRootDir = fullfile('Volumes','ColorShare1','Users', 'Shared', 'Matlab', 'Analysis', 'BLIlluminationDiscriminationCalcs');
     dataBaseDir   = fullfile(sharedRootDir, 'Data');
+    
+    % If the preferences group already exists, remove it
     if (ispref('BLIlluminationDiscriminationCalcs'))
         rmpref('BLIlluminationDiscriminationCalcs');
     end
+    
+    % Set preferences
     setpref('BLIlluminationDiscriminationCalcs', 'SharedRootDir', sharedRootDir);
     setpref('BLIlluminationDiscriminationCalcs', 'DataBaseDir', dataBaseDir);
-    
 end
 
 
