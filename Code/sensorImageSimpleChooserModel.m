cc9fb987d11cbddc76e7e51ee057fde7996c5283
David Brainard <brainard@psych.upenn.edu>
2015-03-25 09:47:56 -0400

    Mucking around a bit.  And, wrote a threshold simulator that can be tuned up.
    Signed-off-by: David Brainard <brainard@psych.upenn.edu>

--- Code/sensorImageSimpleChooserModel.m
+++ Code/sensorImageSimpleChooserModel.m
@@ -17,7 +17,9 @@ function sensorImageSimpleChooserModel
     % Load scene to get FOV
     scene = loadSceneData('Standard', 'TestImage0');
     fov = sceneGet(scene, 'fov');
-    
+    scene = sceneSet(scene,'fov',fov/4);
+    fov = sceneGet(scene, 'fov');
+
     % load oi for FOV
     oi = loadOpticalImageData('Standard', 'TestImage0');
     
@@ -25,16 +27,55 @@ function sensorImageSimpleChooserModel
     sensor = sensorCreate;
 %     sensor = sensorSet(sensor, 'noise flag', 1);
     sensor = sensorSet(sensor,'exp time',0.050);
-%     [sensor, ~] = sensorSetSizeToFOV(sensor,fov,scene,oi);
+    [sensor, ~] = sensorSetSizeToFOV(sensor,fov,scene,oi);
     sensor = sensorSet(sensor, 'wavelength', SToWls([380 8 51]));
+    
+    % Simulate trials
+    nSimulatedTrials = 100;
+    correct = zeros(nSimulatedTrials,1);
+    for t = 1:nSimulatedTrials
+        if (rem(t,10) == 0)
+            fprintf('Finished %d of %d simulated trials\n',t,nSimulatedTrials);
+        end
+        
+        % We need one sample for the initially presented reference standard image
+        voltsStandardReference = getNoisySensorImages('Standard','TestImage0',sensor,1);
+        
+        % We need one sample for the comparison version of the standard image
+        voltsStandardComparison = getNoisySensorImages('Standard','TestImage0',sensor,1);
+        
+        % We need one sample for the presented version of the test image
+        %voltsTestComparison = getNoisySensorImages('BlueIllumination','blue1L-RGB',sensor,1);
+        voltsTestComparison = getNoisySensorImages('Standard','TestImage0',sensor,1);
+        
+        if (t == 1)
+            figure; clf;
+            imshow(voltsStandardReference/max(voltsStandardReference(:)));
+            figure; clf;
+            imshow(voltsStandardComparison/max(voltsStandardComparison(:)));
+            figure; clf;
+            imshow(voltsTestComparison/max(voltsTestComparison(:)));
+            drawnow;
+        end
+        
+        % Figure out which comparison is closer to standard
+        distToStandard = norm(voltsStandardReference(:)-voltsStandardComparison(:));
+        distToTest = norm(voltsStandardReference(:)-voltsTestComparison(:));
+        
+        % Decide if 'subject' was correct on this trial
+        if (distToStandard < distToTest)
+            correct(t) = 1;
+        else
+            correct(t) = 0;
+        end
+    end
+    fprintf('Percent correct = %d\n',round(100*sum(correct)/length(correct)));
     
     % Get noisy sensor images
     k = 10;
     volts = getNoisySensorImages('Standard', 'TestImage0', sensor, k);
+    
  
-    % Get standard sensor to compare against
-    sensor = sensorCompute(sensor, oi);
-    baseVolt = sensorGet(sensor, 'volts');
     
     folderList = {'BlueIllumination', 'GreenIllumination', ...
         'RedIllumination', 'YellowIllumination'};
