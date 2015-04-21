fd15df015d1a427133150dbf894b71959b2b8b8d
xnieamo <xiaomao_ding@yahoo.com>
2015-04-21 19:54:07 -0400

    Added threshholds for green illumination

--- Code/threshholdCumulativeNormal.m
+++ Code/threshholdCumulativeNormal.m
@@ -1,5 +1,5 @@
-function threshholdCumulativeNormal
-%threshholdCumulativeNormal
+function threshholdBlueCumulativeNormal
+%threshholdBlueCumulativeNormal
 %   This function uses the cumulative normal as a psychometric function to
 %   fit the data calculated from the simple image discrimination chooser
 %   model.  
@@ -11,24 +11,26 @@ function threshholdCumulativeNormal
 
     % load data
     data  = load('blueIllumComparison');
-    matrix = data.matrix;
+    blueMatrix = data.matrix;
+    data  = load('greenIllumComparison');
+    greenMatrix = data.matrix;
     
     % set data
     StimLevels = 1:1:50;
-    NumPos     = matrix(1:50,6)';
     OutofNum   = repmat(100, 1, 50);
     
 
-    % Define estimated threshhold and slope for k = 2 -> 7
-    ThreshEstimate = [4 7 12 20 30 40];
-    SlopeEstimate  = [0.1 0.1 0.1 0.1 0.1 0.1];
-    
+    % Define estimated threshholdBlue and slope for k = 2 -> 7
+    ThreshEstimateBlue = [4 7 12 20 30 40];
+    SlopeEstimateBlue  = [0.1 0.1 0.1 0.1 0.1 0.1];
+     
     % set params
-%     paramsValues = [32, .1, .5, 0];
     paramsFree  = [1, 1, 0, 0];
     criterion = .709;
-    paramsValues = zeros(6, 4);
-    threshhold = zeros(6, 1);
+    
+    % blue data set
+    paramsValuesBlue = zeros(6, 4);
+    threshholdBlue = zeros(6, 1);
 
     % Define PF as cumulative normal
     PF = @PAL_CumulativeNormal;
@@ -39,17 +41,24 @@ function threshholdCumulativeNormal
     options.MaxFunEvals = 10000 * 100;
     options.MaxIter = 500*100;
     
-    % Fit curve
-    for i = 1:6
-        NumPos = matrix(1:50, i + 1)';
-        
-        paramsValues(i,:) = [ThreshEstimate(i) SlopeEstimate(i) 0.5 0];
-        
-        [paramsValues(i,:)] = PAL_PFML_Fit(StimLevels, NumPos, OutofNum, ...
-        paramsValues(i,:), paramsFree, PF, 'SearchOptions', options);
     
-        % Get threshhold
-        threshhold(i) = PFI(paramsValues(i,:), criterion);
+%     NumPos = greenMatrix(1:50, 10)';
+%     params = [45 .1 .5 0];
+%     [ params ] = PAL_PFML_Fit(StimLevels, NumPos, OutofNum, ...
+%         params, paramsFree, PF, 'SearchOptions', options)
+%     thresh = PFI(params, criterion)
+    
+    % Fit curve for blue
+    for i = 1:6
+        NumPos = blueMatrix(1:50, i + 1)';
+        
+        paramsValuesBlue(i,:) = [ThreshEstimateBlue(i) SlopeEstimateBlue(i) 0.5 0];
+        
+        [paramsValuesBlue(i,:)] = PAL_PFML_Fit(StimLevels, NumPos, OutofNum, ...
+        paramsValuesBlue(i,:), paramsFree, PF, 'SearchOptions', options);
+    
+        % Get threshholdBlue
+        threshholdBlue(i) = PFI(paramsValuesBlue(i,:), criterion);
         
         
         % Plot fitted curves
@@ -57,12 +66,12 @@ function threshholdCumulativeNormal
 %         PropCorrectData = NumPos./OutofNum;
 %         StimLevelsFine  = [min(StimLevels):(max(StimLevels)-...
 %             min(StimLevels))/1000:max(StimLevels)];
-%         Fit = PF(paramsValues(i,:), StimLevelsFine);
+%         Fit = PF(paramsValuesBlue(i,:), StimLevelsFine);
 %         plot(StimLevels, PropCorrectData, 'k.', 'markersize', 40);
 %         set(gca, 'fontsize', 12);
 %         hold on;
 %         plot(StimLevelsFine, Fit, 'g-', 'linewidth', 4);
-%         plot([threshhold(i) threshhold(i)], [0, criterion], 'g', 'linewidth', 3);
+%         plot([threshholdBlue(i) threshholdBlue(i)], [0, criterion], 'b', 'linewidth', 3);
 %         
 %         ThisTitle = strcat('K-Value : ',int2str(i+1));
 %         title(ThisTitle);
@@ -71,20 +80,50 @@ function threshholdCumulativeNormal
         
     end
 
+    % Threshholds and slope estimates for greenIllumination
+    ThreshEstimateGreen = [4 10 17 24 30 38 40 45];
+    SlopeEstimateGreen  = [0.1 0.1 0.1 0.1 0.1 0.1 0.1 0.1];
+    paramsValuesGreen = zeros(8,4);
+    threshholdGreen = zeros(8,1);
     
-    
-
-   
+    % Fit curve for green
+    for i = 1:8
+        NumPos = greenMatrix(1:50,i+2)';
         
-    % Plot threshhold against k value
-    kVals = 2:7;
+        paramsValuesGreen(i,:) = [ThreshEstimateGreen(i) SlopeEstimateGreen(i) 0.5 0];
+        
+        [paramsValuesGreen(i,:)] = PAL_PFML_Fit(StimLevels, NumPos, OutofNum, ...
+        paramsValuesGreen(i,:), paramsFree, PF, 'SearchOptions', options);
+    
+        % Get threshholdBlue
+        threshholdGreen(i) = PFI(paramsValuesGreen(i,:), criterion);
+        
+    end
+    
+    totalRange = 1:10;
+    kValsFine = [min(totalRange):(max(totalRange)-min(totalRange))/1000:max(totalRange)];
+    % Plot threshholdGreen against k value
+    kVals = 3:10;
 
     figure;
-    plot(kVals, threshhold, 'k.', 'markersize', 40);
-    p = polyfit(kVals, threshhold', 1);
-    kValsFine = [min(kVals):(max(kVals)-min(kVals))/1000:max(kVals)];
+    plot(kVals, threshholdGreen, 'k.', 'markersize', 40);
+    p = polyfit(kVals, threshholdGreen', 1);
+    
     y = polyval(p, kValsFine);
     hold on;
     plot (kValsFine, y, 'g', 'linewidth', 4);
+        
+    % Plot threshholdBlue against k value
+    kVals = 2:7;
+
+    plot(kVals, threshholdBlue, 'k.', 'markersize', 40);
+    p = polyfit(kVals, threshholdBlue', 1);
+    y = polyval(p, kValsFine);
+    hold on;
+    plot (kValsFine, y, 'b', 'linewidth', 4);
+    
+    title('Threshhold against k-values');
+    xlabel('k-values');
+    ylabel('Threshhold');
 end
 
