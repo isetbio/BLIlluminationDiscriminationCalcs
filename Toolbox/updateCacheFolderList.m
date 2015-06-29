4982fc0f7c2bf0bd451cb75ab9ece07513cbb3b1
xnieamo <xiaomao_ding@yahoo.com>
2015-06-29 10:26:28 -0400

    minor

--- Toolbox/updateCacheFolderList.m
+++ Toolbox/updateCacheFolderList.m
@@ -29,8 +29,7 @@ switch (calcParams.calcIDStr)
         calcParams.cacheFolderList = {'NM1', 'NM1'};
     case {'StaticPhoton_NM2','StaticPhoton_NM2_MatlabRNG'}
         calcParams.cacheFolderList = {'NM2', 'NM2'};
-    case {'StaticPhoton_2','SystemicPercentTestLowIllum_2','StaticPhoton_2_Cosine',...
-            'StaticPhoton_2_UnifNoise'}
+    case {'StaticPhoton_2','SystemicPercentTestLowIllum_2','StaticPhoton_2_Cosine'}
         calcParams.cacheFolderList = {'Neutral', 'Neutral_2'};
     case {'PixelNoiseTest'}
         calcParams.cacheFolderList = {'New Images', 'PixelNoise'};
