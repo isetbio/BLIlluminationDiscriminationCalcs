4cfd18dfdd4766254adb7e3afcf1152f8a0f821c
Xiaomao <xiaomao_ding@yahoo.com>
2015-07-31 16:22:43 -0400

    This should make all the calcParams needed now

--- scripts/MasterCalcParamList.m
+++ scripts/MasterCalcParamList.m
@@ -22,56 +22,162 @@ BaseDir = getpref('BLIlluminationDiscriminationCalcs', 'QueueDir');
 
 %% Boolean options for which sets of calcParams to generate
 CREATE_StaticPhoton_Neutral = true;
+CREATE_StaticPhoton_NM1 = true;
+CREATE_StaticPhoton_NM2 = true;
 
 %% StaticPhoton in the Neutral case
-calcIDStrList = {'StaticPhoton' 'StaticPhoton_2' 'StaticPhoton_3' 'StaticPhoton_4' ...
-    'StaticPhoton_5' 'StaticPhoton_6' 'StaticPhoton_7' 'StaticPhoton_8' ...
-    'StaticPhoton_9' 'StaticPhoton_10' 'StaticPhoton_11' 'StaticPhoton_12' ...
-    'StaticPhoton_13' 'StaticPhoton_14' 'StaticPhoton_15'};
-
-for ii = 1:length(calcIDStrList)
+if (CREATE_StaticPhoton_Neutral)
+    calcIDStrList = {'StaticPhoton' 'StaticPhoton_2' 'StaticPhoton_3' 'StaticPhoton_4' ...
+        'StaticPhoton_5' 'StaticPhoton_6' 'StaticPhoton_7' 'StaticPhoton_8' ...
+        'StaticPhoton_9' 'StaticPhoton_10' 'StaticPhoton_11' 'StaticPhoton_12' ...
+        'StaticPhoton_13' 'StaticPhoton_14' 'StaticPhoton_15'};
     
-    % Define the steps of the calculation that should be carried out.
-    calcParams.CACHE_SCENES = false;
-    calcParams.forceSceneCompute = true; % Will overwrite any existing data.
-    
-    calcParams.CACHE_OIS = false;
-    calcParams.forceOICompute = true;    % Will overwrite any existing data.
-    
-    calcParams.RUN_MODEL = true;
-    calcParams.MODEL_ORDER = 1;          % Which model to run
-    calcParams.chooserColorChoice = 0;   % Which color direction to use (0 means all)
-    calcParams.overWriteFlag = 1;        % Whether or not to overwrite existing data.
-    
-    calcParams.CALC_THRESH = false;
-    calcParams.displayIndividualThreshold = false;
-    
-    % Create each calcParam.  Full detail on the fields can be found in runAllCalculations.m
-    calcParams.calcIDStr = calcIDStrList{ii};
-    calcParams = updateCacheFolderList(calcParams);
-    calcParams = updateCropRect(calcParams);
-    
-    calcParams.S = [380 8 51];
-    calcParams.coneIntegrationTime = 0.050;
-    calcParams.sensorFOV = 0.83;
-    
-    calcParams.numTrials = 500;
-    calcParams.maxIllumTarget = 50;
-    
-    calcParams.numKpSamples = 10;
-    calcParams.KpInterval = 1;
-    calcParams.startKp = 1;
-    
-    calcParams.numKgSamples = 1;
-    calcParams.startKg = 0;
-    calcParams.KgInterval = 1;
-    
-    calcParams.targetImageSetSize = 7;
-    calcParams.comparisonImageSetSize = 1;
+    for ii = 1:length(calcIDStrList)
         
-    % Save the calcParam in the queue folder
-    savePath = fullfile(BaseDir, ['calcParams' calcParams.calcIDStr]);
-    save(savePath, 'calcParams');
+        % Define the steps of the calculation that should be carried out.
+        calcParams.CACHE_SCENES = false;
+        calcParams.forceSceneCompute = true; % Will overwrite any existing data.
+        
+        calcParams.CACHE_OIS = false;
+        calcParams.forceOICompute = true;    % Will overwrite any existing data.
+        
+        calcParams.RUN_MODEL = true;
+        calcParams.MODEL_ORDER = 1;          % Which model to run
+        calcParams.chooserColorChoice = 0;   % Which color direction to use (0 means all)
+        calcParams.overWriteFlag = 1;        % Whether or not to overwrite existing data.
+        
+        calcParams.CALC_THRESH = false;
+        calcParams.displayIndividualThreshold = false;
+        
+        % Create each calcParam.  Full detail on the fields can be found in runAllCalculations.m
+        calcParams.calcIDStr = calcIDStrList{ii};
+        calcParams = updateCacheFolderList(calcParams);
+        calcParams = updateCropRect(calcParams);
+        
+        calcParams.S = [380 8 51];
+        calcParams.coneIntegrationTime = 0.050;
+        calcParams.sensorFOV = 0.83;
+        
+        calcParams.numTrials = 500;
+        calcParams.maxIllumTarget = 50;
+        
+        calcParams.numKpSamples = 10;
+        calcParams.KpInterval = 1;
+        calcParams.startKp = 1;
+        
+        calcParams.numKgSamples = 1;
+        calcParams.startKg = 0;
+        calcParams.KgInterval = 1;
+        
+        calcParams.targetImageSetSize = 7;
+        calcParams.comparisonImageSetSize = 1;
+        
+        % Save the calcParam in the queue folder
+        savePath = fullfile(BaseDir, ['calcParams' calcParams.calcIDStr]);
+        save(savePath, 'calcParams');
+    end
 end
-
-
+%% StaticPhoton in the NM1 case
+if (CREATE_StaticPhoton_NM1)
+    calcIDStrList = {'StaticPhoton_NM1' 'StaticPhoton_NM1_2' 'StaticPhoton_NM1_3' 'StaticPhoton_NM1_4' ...
+        'StaticPhoton_NM1_5' 'StaticPhoton_NM1_6' 'StaticPhoton_NM1_7' 'StaticPhoton_NM1_8' ...
+        'StaticPhoton_NM1_9' 'StaticPhoton_NM1_10' 'StaticPhoton_NM1_11' 'StaticPhoton_NM1_12' ...
+        'StaticPhoton_NM1_13' 'StaticPhoton_NM1_14' 'StaticPhoton_NM1_15'};
+    
+    for ii = 1:length(calcIDStrList)
+        
+        % Define the steps of the calculation that should be carried out.
+        calcParams.CACHE_SCENES = false;
+        calcParams.forceSceneCompute = true; % Will overwrite any existing data.
+        
+        calcParams.CACHE_OIS = false;
+        calcParams.forceOICompute = true;    % Will overwrite any existing data.
+        
+        calcParams.RUN_MODEL = true;
+        calcParams.MODEL_ORDER = 1;          % Which model to run
+        calcParams.chooserColorChoice = 0;   % Which color direction to use (0 means all)
+        calcParams.overWriteFlag = 1;        % Whether or not to overwrite existing data.
+        
+        calcParams.CALC_THRESH = false;
+        calcParams.displayIndividualThreshold = false;
+        
+        % Create each calcParam.  Full detail on the fields can be found in runAllCalculations.m
+        calcParams.calcIDStr = calcIDStrList{ii};
+        calcParams = updateCacheFolderList(calcParams);
+        calcParams = updateCropRect(calcParams);
+        
+        calcParams.S = [380 8 51];
+        calcParams.coneIntegrationTime = 0.050;
+        calcParams.sensorFOV = 0.83;
+        
+        calcParams.numTrials = 500;
+        calcParams.maxIllumTarget = 50;
+        
+        calcParams.numKpSamples = 10;
+        calcParams.KpInterval = 1;
+        calcParams.startKp = 1;
+        
+        calcParams.numKgSamples = 1;
+        calcParams.startKg = 0;
+        calcParams.KgInterval = 1;
+        
+        calcParams.targetImageSetSize = 7;
+        calcParams.comparisonImageSetSize = 1;
+        
+        % Save the calcParam in the queue folder
+        savePath = fullfile(BaseDir, ['calcParams' calcParams.calcIDStr]);
+        save(savePath, 'calcParams');
+    end
+end
+%% StaticPhoton in the NM2 case
+if (CREATE_StaticPhoton_NM2)
+    calcIDStrList = {'StaticPhoton_NM2' 'StaticPhoton_NM2_2' 'StaticPhoton_NM2_3' 'StaticPhoton_NM2_4' ...
+        'StaticPhoton_NM2_5' 'StaticPhoton_NM2_6' 'StaticPhoton_NM2_7' 'StaticPhoton_NM2_8' ...
+        'StaticPhoton_NM2_9' 'StaticPhoton_NM2_10' 'StaticPhoton_NM2_11' 'StaticPhoton_NM2_12' ...
+        'StaticPhoton_NM2_13' 'StaticPhoton_NM2_14' 'StaticPhoton_NM2_15'};
+    
+    for ii = 1:length(calcIDStrList)
+        
+        % Define the steps of the calculation that should be carried out.
+        calcParams.CACHE_SCENES = false;
+        calcParams.forceSceneCompute = true; % Will overwrite any existing data.
+        
+        calcParams.CACHE_OIS = false;
+        calcParams.forceOICompute = true;    % Will overwrite any existing data.
+        
+        calcParams.RUN_MODEL = true;
+        calcParams.MODEL_ORDER = 1;          % Which model to run
+        calcParams.chooserColorChoice = 0;   % Which color direction to use (0 means all)
+        calcParams.overWriteFlag = 1;        % Whether or not to overwrite existing data.
+        
+        calcParams.CALC_THRESH = false;
+        calcParams.displayIndividualThreshold = false;
+        
+        % Create each calcParam.  Full detail on the fields can be found in runAllCalculations.m
+        calcParams.calcIDStr = calcIDStrList{ii};
+        calcParams = updateCacheFolderList(calcParams);
+        calcParams = updateCropRect(calcParams);
+        
+        calcParams.S = [380 8 51];
+        calcParams.coneIntegrationTime = 0.050;
+        calcParams.sensorFOV = 0.83;
+        
+        calcParams.numTrials = 500;
+        calcParams.maxIllumTarget = 50;
+        
+        calcParams.numKpSamples = 10;
+        calcParams.KpInterval = 1;
+        calcParams.startKp = 1;
+        
+        calcParams.numKgSamples = 1;
+        calcParams.startKg = 0;
+        calcParams.KgInterval = 1;
+        
+        calcParams.targetImageSetSize = 7;
+        calcParams.comparisonImageSetSize = 1;
+        
+        % Save the calcParam in the queue folder
+        savePath = fullfile(BaseDir, ['calcParams' calcParams.calcIDStr]);
+        save(savePath, 'calcParams');
+    end
+end
