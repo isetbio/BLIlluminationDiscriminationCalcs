078273d7493ed01a1bb91a687dfcecc3c8056c8e
Xiaomao Ding <xiaomaoding@seal.psych.upenn.edu>
2016-06-02 12:00:35 -0400

    Turning code into a loop slowly

--- tutorials/temporary/PlotResults.m
+++ tutorials/temporary/PlotResults.m
@@ -1,12 +1,26 @@
-%% load
+% PlotResults
+% 
+% This function was written to be used in conjunction with
+% ComparingClassifiersWithData. This is not guaranteed to work with data
+% generated through other means.
+
+%% Load data
 clear;
-dataToLoad = 'ClassifierAnalysis_250_250_std.mat';
-dataPath = fullfile(getpref('BLIlluminationDiscriminationCalcs', 'AnalysisDir'), 'ClassifierComparisons');
-load(fullfile(dataPath,dataToLoad));
-allData = {NNpercentCorrect, DApercentCorrect, SVMpercentCorrect};
-thresholdToExtract = 5; saveThresholds = false;
+
+% dataToLoad specifies the file name(s) to load.
+dataToLoad = {'ClassifierAnalysis_100_100_std_NM2_Mean.mat' 'ClassifierAnalysis_500_500_std_NM1_Mean.mat' ...
+    'ClassifierAnalysis_500_500_std_Neutral_Mean.mat'};
+
+% This variable determines the INDEX of the threshold to extract. That is,
+% if the noise levels are [1 3 5], then setting the value to 2 will extract
+% the thresholds for noise level 3.
+thresholdToExtract = [3 3 6]; saveThresholds = false;
 
 %% Plot
+
+% DO NOT TOUCH THESE VARIABLES. These are settings that are supposed to be
+% constant and representative of experimental conditions and/or the
+% organizational structure of the plotting code.
 ColorMapping = containers.Map({'Blue' 'Green' 'Yellow' 'Red'},{'b.-' 'g.-' 'y.-' 'r.-'});
 
 cl = {'NN' 'DA' 'SVM'};
@@ -23,40 +37,52 @@ options.TolFun = 1e-09;
 options.MaxFunEvals = 10000 * 100;
 options.MaxIter = 500*100;
 
-extractedThresholds = zeros(4,1);
-
-figure;
-for kk = 1:3
-    subplot(1,3,kk);
-    hold on;
-    title(cl{kk});
-    dataset = allData{kk};
-    for ii = 1:length(Colors)
-        threshold = zeros(numKValue, 1);
-        paramsValues = zeros(numKValue, 4);
-        data = dataset(:,:,ii);
-        for jj = 1:numKValue
-            [paramsValues(jj,:)] = PAL_PFML_Fit(stimLevels', data(:,jj), outOfNum',  paramsValueEst, paramsFree, PF, 'SearchOptions', options);
-            threshold(jj) = PF(paramsValues(jj,:), criterion, 'inverse');
+% Actual code to plot the data
+for nn = 1:length(dataToLoad)
+    % Load and format the data appropriately
+    dataPath = fullfile(getpref('BLIlluminationDiscriminationCalcs', 'AnalysisDir'), 'ClassifierComparisons');
+    load(fullfile(dataPath,dataToLoad{nn}));
+    allData = {NNpercentCorrect, DApercentCorrect, SVMpercentCorrect};
+   
+    % Check for meta-data related to the calculation
+    if ~exist('NoiseSteps','var') || ~exist('Colors','var') , error('Faulty dataset! Missing metadata.'); end
+    
+    extractedThresholds = zeros(4,3);
+    
+    figure;
+    for kk = 1:3
+        subplot(1,3,kk);
+        hold on;
+        title(cl{kk});
+        dataset = allData{kk};
+        tN = thresholdToExtract(kk);
+        for ii = 1:length(Colors)
+            threshold = zeros(numKValue, 1);
+            paramsValues = zeros(numKValue, 4);
+            data = dataset(:,:,ii);
+            for jj = 1:numKValue
+                [paramsValues(jj,:)] = PAL_PFML_Fit(stimLevels', data(:,jj), outOfNum',  paramsValueEst, paramsFree, PF, 'SearchOptions', options);
+                threshold(jj) = PF(paramsValues(jj,:), criterion, 'inverse');
+            end
+            
+            % Pick out the target of extraction
+            extractedThresholds(ii,kk) = threshold(tN);
+            if ii == 4, plot([NoiseSteps(tN) NoiseSteps(tN)], [0 50], 'k'); end
+            
+            plotIdx = threshold > 0.001; % Float equality check
+            plot(NoiseSteps(plotIdx), threshold(plotIdx), ColorMapping(Colors{ii}), 'markersize', 35);
         end
-        
-        % Pick out the target of extraction
-        if kk == 1
-            extractedThresholds(ii) = threshold(thresholdToExtract);
-            if ii == 4, plot([thresholdToExtract thresholdToExtract], [0 50], 'k'); end
-        end
-        
-        plot(1:10, threshold, ColorMapping(Colors{ii}), 'markersize', 35);
+        plot([0 NoiseSteps(end) + 1], [10 10], 'k--');
+        ylim([0 50]);
+        xlim([0 NoiseSteps(end) + 1]);
+        xlabel('Noise Factor');
+        ylabel('Threshold');
+        axis square;
     end
-    ylim([0 50]);
-    xlabel('Noise Factor');
-    ylabel('Threshold');
-    axis square;
+    suplabel(strrep(dataToLoad{nn},'_','\_'), 't');
+    
+    if saveThresholds, save([dataToLoad(1:end-4) 'ExtThresh.mat'],'extractedThresholds','Colors'); end
 end
-suptitle(strrep(dataToLoad,'_','\_'));
-
-if saveThresholds, save([dataToLoad(1:end-4) 'ExtThresh.mat'],'extractedThresholds','Colors'); end
-
 %% Visualize the PCA's
 stimLevelToPlot = [1,10,25,50];
 noiseLevel = 1;
