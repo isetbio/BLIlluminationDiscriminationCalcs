d515e90df961776d86fde7b16f81e1e477f63921
Xiaomao Ding <xiaomaoding@seal.psych.upenn.edu>
2016-06-17 15:32:30 -0400

    Started some changes

--- toolbox/Utilities/resizeOI.m
+++ toolbox/Utilities/resizeOI.m
@@ -12,8 +12,12 @@ function newoi = resizeOI(oi, fov)
 % xd  4/22/2016 wrote it
 % xd  5/19/2016 added comments and formatting
 
-%% Use 0 to leave oi unmodified
+%% Use 0 to leave OI unmodified
 if fov == 0, newoi = oi; return; end;
+
+%% Average the OI differently from resizing it
+
+
 
 %% Calculation if fov not 0
 % Get the old fov to calculate how many data points to average for the new oi
