4bca4620c89a17542e614a221e3bfd6e9f784ab1
Xiaomao Ding <xiaomaoding@seal.psych.upenn.edu>
2016-06-20 09:59:27 -0400

    Fixed a comment

--- toolbox/ClassifierFunctions/cf3_SupportVectorMachine.m
+++ toolbox/ClassifierFunctions/cf3_SupportVectorMachine.m
@@ -2,7 +2,7 @@ function [percentCorrect,svm] = cf3_SupportVectorMachine(trainingData, testingDa
 % percentCorrect = cf3_SupportVectorMachine(trainingData, testingData, trainingClasses, testingClasses)
 %
 % This function classifies testingData using a SVM with no optimization of 
-% the classifiers hyperparameters. See fitcsvm for more details on SVM.
+% the classifier's hyperparameters. See fitcsvm for more details on SVM.
 %
 % xd  5/26/16  wrote it
 
