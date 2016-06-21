d289fe73382ea47544d82739c453f74409935949
Xiaomao Ding <xiaomaoding@modv-vlan533.0334.apn.wlan.wireless-pennnet.upenn.edu>
2016-06-21 16:44:28 -0400

    Started reworking the plotting code to make it easier
    Kept old code in xOLD folder for reference and emergencies. Will be deleted upon completion.

---
+++ toolbox/Thresholds/createPlotInfoStruct.m
@@ -0,0 +1,18 @@
+function plotInfo = createPlotInfoStruct
+% plotInfo = createPlotInfoStruct
+% 
+% This function generates a default plotInfo struct which is passed around
+% and used to plot various data in this project.
+%
+% xd  6/21/16  wrote it
+
+plotInfo.fitColor = [];
+plotInfo.stimLevels = [];
+
+plotInfo.xlabel = 'Stimulus Levels';
+plotInfo.ylabel = 'Percent Correct';
+plotInfo.title = 'Weibull Fit to Data';
+plotInfo.criterion = 70.9;
+
+end
+
---
+++ toolbox/Thresholds/plotFitForSingleThreshold.m
@@ -0,0 +1,48 @@
+function plotFitForSingleThreshold(plotInfo,data,threshold,fitParams)
+% plotFitForSingleThreshold(plotInfo,data,fitParams,threshold)
+% 
+% This function will generate a plot for the fitted Weibull function as
+% well as the data used to fit. threshold and fitParams are the direct
+% outputs of the singleThresholdExtraction function. data is the data
+% points used to generated the fit. plotInfo is a struct that contains
+% information for that will be used to add details such as the axis labels
+% and title.
+%
+% xd  6/21/16  wrote it
+
+%% Generate a default set of figure parameters used in this project
+figParams = BLIllumDiscrFigParams([],'sThreshold');
+
+% We'll also extract some parameters based on what is specified in the
+% plotInfo struct here.
+if isempty(plotInfo.stimLevels), plotInfo.stimLevels = 1:length(data); end;
+if ~isempty(plotInfo.fitColor), figParams.defaultFitLineColor = plotInfo.fitColor; end;
+
+% Create a finely spaced data vector to generate data points along a curve for plotting.
+stimLevelsFine = min(plotInfo.stimLevels):(max(plotInfo.stimLevels)-min(plotInfo.stimLevels))/1000:max(plotInfo.stimLevels);
+curveFit = PAL_Weibull(fitParams,stimLevelsFine) * 100;
+
+%% Create the plot
+figure('Position',figParams.sqPosition); hold on;
+
+plot(plotInfo.stimLevels,data,figParams.dataMarker,'MarkerSize',figParams.dataMarkerSize); 
+plot(stimLevelsFine,curveFit,'Color',figParams.defaultFitLineColor, 'LineWidth',figParams.fitLineWidth);
+plot([threshold threshold], [0 plotInfo.criterion],'Color',figParams.defaultCriterionColor,'LineWidth',figParams.criterionLineWidth);
+plot([min(stimLevelsFine) threshold], [plotInfo.criterion plotInfo.criterion],'Color',figParams.defaultCriterionColor,...
+    'LineStyle',figParams.criterionLineStyle,'LineWidth',figParams.criterionLineWidth);
+
+% Do some plot manipulations to make it look nice
+set(gca,'FontName',figParams.fontName,'FontSize',figParams.axisFontSize,'LineWidth',figParams.axisLineWidth);
+axis square;
+ylim(figParams.ylimit);
+
+xl = xlabel(plotInfo.xlabel);
+yl = ylabel(plotInfo.ylabel);
+t = title(plotInfo.title);
+yl.Position = yl.Position + figParams.deltaYlabelPosition;
+xl.Position = xl.Position + figParams.deltaXlabelPosition;
+
+
+
+end
+
