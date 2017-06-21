8ebf61e64c50d48ed647e66ef27fcef05e80e08c
Xiaomao Ding <xiaomaoding@seal.psych.upenn.edu>
2017-06-21 14:18:09 -0400

    Working on the one script

--- scripts/PaperCalculations/AllPaperCalculations.m
+++ scripts/PaperCalculations/AllPaperCalculations.m
@@ -12,35 +12,46 @@ numExecutables = 5;
 PATH = getenv('PATH');
 setenv('PATH',[PATH ':' matlabroot '/bin']);
 
-%%
-system('matlab -nojvm -nodesktop -r "disp(''hi''); exit;"');
+%% Run the calculation code
 
 % Generate all the optical images. We assume that the file directory has
 % been set up appropriately?
-makeAllOISets(coresForCreatingOI);
+% makeAllOISets(coresForCreatingOI);
 
 % Create a list of partial calcParams. This gets looped over to execute all
 % the calculations.
 calcParamsList = {};
-calcParamsList{1} = struct('cacheFolderList',{'Constant','Constant_FullImage'},'spatialDensity',[0 0.62 0.31 0.07]);
-calcParamsList{2} = struct('cacheFolderList',{'Constant','Constant_FullImage'},'spatialDensity',[0 0.93 0.00 0.07]);
-calcParamsList{3} = struct('cacheFolderList',{'Constant','Constant_FullImage'},'spatialDensity',[0 0.00 0.93 0.07]);
-calcParamsList{4} = struct('cacheFolderList',{'Constant','Constant_FullImage'},'spatialDensity',[0 0.66 0.34 0.00]);
-calcParamsList{5} = struct('cacheFolderList',{'Constant','Constant_FullImage'},'spatialDensity',[0 1.00 0.00 0.00]);
-calcParamsList{6} = struct('cacheFolderList',{'Constant','Constant_FullImage'},'spatialDensity',[0 0.00 1.00 0.00]);
-calcParamsList{7} = struct('cacheFolderList',{'Constant','Constant_FullImage'},'spatialDensity',[0 0.00 0.00 1.00]);
+calcParamsList{1} = setfield(struct('spatialDensity',[0 0.62 0.31 0.07]),'cacheFolderList',{'Constant','Constant_FullImage'});
+calcParamsList{2} = setfield(struct('spatialDensity',[0 0.93 0.00 0.07]),'cacheFolderList',{'Constant','Constant_FullImage'});
+calcParamsList{3} = setfield(struct('spatialDensity',[0 0.00 0.93 0.07]),'cacheFolderList',{'Constant','Constant_FullImage'});
+calcParamsList{4} = setfield(struct('spatialDensity',[0 0.66 0.34 0.00]),'cacheFolderList',{'Constant','Constant_FullImage'});
+calcParamsList{5} = setfield(struct('spatialDensity',[0 1.00 0.00 0.00]),'cacheFolderList',{'Constant','Constant_FullImage'});
+calcParamsList{6} = setfield(struct('spatialDensity',[0 0.00 1.00 0.00]),'cacheFolderList',{'Constant','Constant_FullImage'});
+calcParamsList{7} = setfield(struct('spatialDensity',[0 0.00 0.00 1.00]),'cacheFolderList',{'Constant','Constant_FullImage'});
 
-calcParamsList{8}  = struct('cacheFolderList',{'Neutral','Neutral_FullImage'},'spatialDensity',[0 0.62 0.31 0.07]);
-calcParamsList{9}  = struct('cacheFolderList',{'NM1','NM1_FullImage'},'spatialDensity',[0 0.62 0.31 0.07]);
-calcParamsList{10} = struct('cacheFolderList',{'NM2','NM2_FullImage'},'spatialDensity',[0 0.62 0.31 0.07]);
+calcParamsList{8}  = setfield(struct('spatialDensity',[0 0.62 0.31 0.07]),'cacheFolderList',{'Neutral','Neutral_FullImage'});
+calcParamsList{9}  = setfield(struct('spatialDensity',[0 0.62 0.31 0.07]),'cacheFolderList',{'NM1','NM1_FullImage'});
+calcParamsList{10} = setfield(struct('spatialDensity',[0 0.62 0.31 0.07]),'cacheFolderList',{'NM2','NM2_FullImage'});
 
 % Run number of scripts according to params with number of parallel cores.
 
 for ii = 1:length(calcParamsList)
+    
+    % Set up strings for vars
+    cacheFolderList = calcParamsList{ii}.cacheFolderList;
+    oiFolder        = calcParamsList{ii}.cacheFolderList{1};
+    sceneFolder     = calcParamsList{ii}.cacheFolderList{2};
+    spatialDensity  = mat2str(calcParamsList{ii}.spatialDensity);
+    numCores = num2str(coresPerExecutable);
+    
     for jj = 1:numExecutables
-        % Set up strings for vars
+        command = ['matlab -nojvm -nodesktop -nodisplay -r "runAllFirstOrderCalcs(' ...
+                   numCores ',' oiFolder ',' sceneFolder ',' spatialDensity ...
+                   '); exit;" >output.txt 2>error.txt &'];
         
         % Execute from command line?
+%         system(command);
+        disp(command)
         
     end
     
@@ -51,6 +62,7 @@ for ii = 1:length(calcParamsList)
     
 end
 
+%% Run the analysis code
 
 
 
